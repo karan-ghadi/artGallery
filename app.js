d6f657a204f2cedf4ac56a0c544d088444ce05c8
Karan Ghadi <karan@esscale.co>
2018-10-09 13:31:35 +0530

    basic init files

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const databseUrl = 'mongodb://localhost:27017/artGallery'
+
+mongoose.connect(databseUrl, {
+	useNewUrlParser: true
+});
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+app.use(bodyParser.json());
+
+// setting headers
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+	if (req.method === 'OPTIONS') {
+		res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+		return res.status(200).json({});
+	}
+	next();
+})
+
+app.get('/users', (req, res, next) => {
+	res.status(200).json({
+		message: 'connected successfully'
+	})
+});
+
+/* Error Handling */
+app.use((req, res, next) => {
+	console.log('error message');
+	const error = new Error('Not Found');
+	error.status = 404;
+	next(error);
+})
+app.use((error, req, res, next) => {
+	res.status(error.status || 500).json({
+		error: {
+			global: error.message
+		}
+	})
+})
+
+module.exports = app;
